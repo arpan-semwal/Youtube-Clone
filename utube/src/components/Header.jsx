c73ce9409698a8c727fb42c9dc7a95a37e6fa5f2
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-06-27 16:57:17 +0530

    update Header.jsx

--- utube/src/components/Header.jsx
+++ utube/src/components/Header.jsx
@@ -29,10 +29,11 @@ const Header = () => {
     const pageName = pathname.split("/").filter(Boolean)?.[0];
 
   return (
-    <div className='sticky top-0 z-10 flex flex-row items-center h-14 px-4 md:px-5 bg-white dark:bg-black'>
+    <div className='sticky top-0 z-10 flex flex-row items-center h-14 px-4 md:px-5 bg-white dark:bg-black justify-between'>
       {loading && <Loader/>} 
 
       {/* for menubar  */}
+
       <div className='flex h-5 items-center'>
         {pageName !=="video" && (
           <div className='flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]' onClick={mobileMenuToggle}>
@@ -56,35 +57,46 @@ const Header = () => {
     </div>
 
 
-      {/* seacrch bar  */}
+
+      {/* search bar  */}
 
 
-      <div className="group flex items-center ml-auto">
+      <div className="flex items-center justify-center flex-grow">
         <div className='flex h-8 md:h-10  border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0'>
           <div className='w-10 items-center justify-center hidden group-focus-within:flex'>
             <IoIosSearch className="text-white text-xl"/>
           </div>
           <input 
           type='text' 
-          className='bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w[500px]' 
+          className='bg-transparent outline-none text-white pl-5 md:pl-5  w-64 md:w-96 lg:w-[500px]' 
           onChange={(e) => setSearchQuery(e.target.value)}
           onKeyUp={searchQueryHandler}
           value={searchQuery}
           />
         </div>
+
+
          {/* search button */}
-         <button className='w--[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-tr-3xl bg-white/[0.1]'>
+
+         <button className='w--[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-tr-4xl bg-white/[0.1]' >
             <IoIosSearch className="text-white text-xl"/>
          </button>
       </div>
+
+
+
+      {/* video and icon button */}
 
       <div className='flex items-center'>
         <div className='hidden md:flex'>
               <div className='flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]'>
               <RiVideoAddLine className='text-white text-xl cursor-pointer'/>
               </div>
-              <div>
+              <div className='flex items-center justify-center  ml-2 h-10 w-10 rounded-full hover:bg-[#3030303]/0.6'>
                 <FiBell className='text-white text-xl cursor-pointer'/>
+              </div>
+              <div className='flex h-8 w-8 overflow-hidden rounded-full md:ml-4'>
+                  <img src='https://robohash.org/stefan-one'/>
               </div>
         </div>
       </div>
