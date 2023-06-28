245aa9bece7e407e58acf11608fd10cd94be9b3e
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-06-28 15:30:01 +0530

    2

--- utube/src/components/Feed.jsx
+++ utube/src/components/Feed.jsx
@@ -7,20 +7,21 @@ const Feed = () => {
   const { loading, searchResults } = useContext(Context);
 
   useEffect(() => {
-    document.getElementById('root').classList.remove('custopm-h');
+    const rootElement = document.getElementById('root');
+    if (rootElement) {
+      rootElement.classList.remove('custopm-h');
+    }
   }, []);
 
   return (
-    <div className="flex flex-row h-[calc(100% - 56px)]">
+    <div className="flex flex-row h-screen">
       <LeftNav />
-      <div className="grow w-[calc(100% - 240px)] h-full overflow-y-auto bg-black">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
-          {Array.isArray(searchResults) &&
-            searchResults.map((item) => {
-              if (item.type !== "video") return null;
-              return <VideoCard key={item?.video?.videoId
-              } video={item?.video} />;
-            })}
+      <div className="flex-grow bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5 h-full overflow-y-auto">
+          {!loading && Array.isArray(searchResults) && searchResults.map((item) => {
+            if (item.type !== "video") return null;
+            return <VideoCard key={item?.video?.videoId} video={item?.video} />;
+          })}
         </div>
       </div>
     </div>
--- utube/src/components/VideoCard.jsx
+++ utube/src/components/VideoCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import{BsFillCheckCircleFill} from "react-icons/bs";
+import {abbreviateNumber} from "js-abbreviation-number";
 
-const VideoCard = () => {
+const VideoCard = ({video}) => {
   return (
+    // <Link to={`/video/${video.videoId}`}>
+    //   <div className='flex flex-col mb-8'>
+    //     <div className='relative h-48 md:h-40 md:rounded-xl overflow-hidden'>
+    //       <img className="h-full w-full object-cover" 
+    //       src={video?.thumbnails?.[0]?.url}/>
+    //     </div>
+    //   </div>
+    // </Link>
     <div>
-      VideoCarddsdasda
+      hello
     </div>
   )
 }
