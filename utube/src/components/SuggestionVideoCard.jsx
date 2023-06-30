927a8d0b5af72e0e792734879d7e7592a46a69d3
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-06-30 12:15:49 +0530

    suggestioncard

--- utube/src/components/SuggestionVideoCard.jsx
+++ utube/src/components/SuggestionVideoCard.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import VideoLength from '../shared/VideoLength';
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { abbreviateNumber } from "js-abbreviation-number";
 
-const SuggestionVideoCard = () => {
+const SuggestionVideoCard = ({ video }) => {
+  console.log(video);
+
   return (
-    <div>
-      SuggestionVideoCard
-    </div>
-  )
-}
+    <Link to={`/video/${video?.videoId}`}>
+      <div className='flex mb-3 overflow-hidden w-full h-full'>
+        <div className='relative overflow-hidden lg:h-20 xl:h-24 w-40 min-w-[168px] lg:w-32 lg:min-w-[128px] xl:w-40 xl:min-w-[168px] rounded-xl bg-slate-800'>
+          <img className='h-full w-full object-cover' src={video?.thumbnails?.[0]?.url} alt="Thumbnail" />
+          {video?.lengthSeconds && (
+            <VideoLength time={video?.lengthSeconds} />
+          )}
+        </div>
+        <div className='flex flex-col ml-3 sm:mb-10 xl:mb-10 overflow-hidden'>
+          <span className='text-sm ld:text-xs xl:text-sm font-bold line-clamp-2 text-white'>
+            {video?.title}
+          </span>
+          <span className='text-[12px] lg:text-[10px] xl:text-[12px] font-semibold mt-2 text-white/[0.7] flex items-center'>
+            {video.author.title}
+            {video?.author?.badges?.[0]?.type === "VERIFIED_CHANNEL" && (
+              <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] lg:text-[10px] xl:text-[12px] ml-1" />
+            )}
+          </span>
+          {/* for views */}
+          <div className='flex text-[12px] lg:text-[10px] xl:text-[12px] font-semibold text-white/[0.7] truncate overflow-hidden'>
+            <span>{`${abbreviateNumber(video?.stats?.views, 2)}`}</span>
+          </div>
+          <span className='truncate'>
+            {video?.publishedTimeText}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default SuggestionVideoCard
+export default SuggestionVideoCard;
--- utube/src/components/VideoDetails.jsx
+++ utube/src/components/VideoDetails.jsx
@@ -1,40 +1,34 @@
-import React , {useState , useEffect , useContext} from 'react';
-import {BsFillCheckCircleFill} from "react-icons/bs";
-import {abbreviateNumber} from "js-abbreviation-number";
+import React, { useState, useEffect, useContext } from 'react';
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { abbreviateNumber } from "js-abbreviation-number";
 import ReactPlayer from "react-player/youtube";
-import {AiOutlineLike} from "react-icons/ai";
-import {useParams} from "react-router-dom";
-import {fetchDataFromApi} from "../utils/api";
+import { AiOutlineLike } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import { fetchDataFromApi } from "../utils/api";
 import { Context } from "../context/contextApi";
 import SuggestionVideoCard from './SuggestionVideoCard';
 
-
-
-
 const VideoDetails = () => {
-
-  const [video , seVideo] = useState();
-  const [relatedVideos , setRelatedVideos] = useState();
-  const{id} = useParams();
-  const {setLoading} = useContext(Context);
-
+  const [video, setVideo] = useState();
+  const [relatedVideos, setRelatedVideos] = useState();
+  const { id } = useParams();
+  const { setLoading } = useContext(Context);
 
   useEffect(() => {
-    document.getElementById("root").classList.add("custom-h"),
-     fetchVideoDetails(),
-     fetchRelatedVideos()
+    document.getElementById("root").classList.add("custom-h");
+    fetchVideoDetails();
+    fetchRelatedVideos();
+  }, [id]);
 
-  } , [id]);
-
-//  For video that we have click to play 
+  // Fetch video details for the video that we have clicked to play
   const fetchVideoDetails = () => {
     setLoading(true);
     fetchDataFromApi(`video/details/?id=${id}`).then((res) => {
       console.log(res);
-      setRelatedVideos(res);
+      setVideo(res);
       setLoading(false);
-    })
-  }
+    });
+  };
 
   const fetchRelatedVideos = () => {
     setLoading(true);
@@ -43,33 +37,69 @@ const VideoDetails = () => {
       setRelatedVideos(res);
       setLoading(false);
     });
-  }
-
-
+  };
 
   return (
-
-    // for video player
     <div className='flex justify-center flex-row h-[calc(100%-56px)] bg-black'>
       <div className='w-full max-w-[1280px] flex flex-col lg:flex-row'>
         <div className='flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto'>
           <div className='h-[200px] md:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0'>
-                <ReactPlayer url={``}
-                 controls
-                  width="100%"
-                  height="100%"
-                  style={{backgroundColor: "#000000"}}
-          />
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              controls
+              width="100%"
+              height="100%"
+              style={{ backgroundColor: "#000000" }}
+            />
+          </div>
+          <div className='text-white font-bold text-sm md:text-xl mt-4 line-clamp-2 '>
+            {video?.title}
+          </div>
+
+          {video && (
+            <div className='flex justify-between flex-col md:flex-row mt-4'>
+              <div className='flex'>
+                <div className="flex items-start">
+                  <div className='flex h-11 w-11 rounded-full overflow-hidden'>
+                    <img className='h-full w-full object-cover' src={video.author?.avatar[0]?.url} alt="Author Avatar" />
+                  </div>
+                </div>
+                <div className='flex flex-col ml-3'>
+                  <div className='text-white text-md font-semibold flex items-center'>
+                    {video.author?.title}
+                    {video.author?.badges?.[0]?.type === "VERIFIED_CHANNEL" && (
+                      <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
+                    )}
+                  </div>
+                  <div className='text-white'>
+                    {video.author?.stats?.subscribersText}
+                  </div>
+                </div>
+              </div>
+              <div className='flex text-white mt-4 md:mt-8'>
+                <div className='flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]'>
+                  <AiOutlineLike className='text-xl text-white mr-2' />
+                  <span>{`${abbreviateNumber(video.stats?.likes, 2)}`}</span>
+                </div>
+                <div className='flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] '>
+                  <AiOutlineLike className='text-xl text-white mr-2' />
+                  <span>{`${abbreviateNumber(video.stats?.views, 2)}`}</span>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-
-
-        <div className='text-white font-bold text-sm md:text-xl mt-4 line-clamp '>
-          {video?.title}
+        <div className='flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]'>
+          {relatedVideos?.contents && relatedVideos.contents.map((items, index) => {
+            if (items.type !== "video") return false;
+            return (
+              <SuggestionVideoCard key={index} video={items.video} />
+            )
+          })}
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default VideoDetails
+export default VideoDetails;
